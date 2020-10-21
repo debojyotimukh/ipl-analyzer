267fcb564a984f87d2b0376e295a173a4f42b17c
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-21 13:23:46 +0530

    refctored to take Strike rates

--- src/main/java/com/capgemini/workshop/ipl/IPLPlayer.java
+++ src/main/java/com/capgemini/workshop/ipl/IPLPlayer.java
@@ -3,26 +3,30 @@ package com.capgemini.workshop.ipl;
 import com.opencsv.bean.CsvBindByName;
 
 public class IPLPlayer {
-    @CsvBindByName(column = "Player",required = true)
+    @CsvBindByName(column = "Player", required = true)
     private String playerName;
 
-    @CsvBindByName(column = "Inns",required = true)
+    @CsvBindByName(column = "Inns", required = true)
     private int innings;
 
-    @CsvBindByName(column="Runs",required = true)
+    @CsvBindByName(column = "Runs", required = true)
     private int runs;
 
-    @CsvBindByName(column="Avg")
+    @CsvBindByName(column = "Avg",required = true)
     private String average;
+
+    @CsvBindByName(column = "SR", required = true)
+    private double strikingRate;
 
     public IPLPlayer() {
     }
 
-    public IPLPlayer(String playerName, int innings, int runs, String average) {
+    public IPLPlayer(String playerName, int innings, int runs, String average, double strikingRate) {
         this.playerName = playerName;
         this.innings = innings;
         this.runs = runs;
         this.average = average;
+        this.strikingRate = strikingRate;
     }
 
     public String getName() {
@@ -50,13 +54,21 @@ public class IPLPlayer {
     }
 
     public Double getAverage() {
-        if(!average.equals("-"))
+        if (!average.equals("-"))
             return Double.parseDouble(average);
         return 0.0;
     }
 
     public void setAverage(String average) {
-        this.average=average;
+        this.average = average;
+    }
+
+    public double getStrikingRate() {
+        return strikingRate;
+    }
+
+    public void setStrikingRate(double strikingRate) {
+        this.strikingRate = strikingRate;
     }
 
 }
