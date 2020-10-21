4b3100ce7c7c9b9a16221594bf780ecb11717597
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-21 13:09:57 +0530

    refactor: to delegate csv operation to PlayerRepository

--- src/main/java/com/capgemini/workshop/ipl/IPLAnalyzer.java
+++ src/main/java/com/capgemini/workshop/ipl/IPLAnalyzer.java
@@ -1,51 +1,33 @@
 package com.capgemini.workshop.ipl;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.capgemini.training.iohelper.CSVBuilderException;
-import com.capgemini.training.iohelper.CSVBuilderFactory;
-import com.capgemini.training.iohelper.ICSVBuilder;
-
 public class IPLAnalyzer {
-	private String csvPath;
+	private PlayerRepository<IPLPlayer> batsman;
 
-	private static <T> List<T> sortBy(final String filepath, final Comparator<T> comparator, final Class<T> clazz)
-			throws IPLAnalyzerException {
+	private static <T> List<T> sortBy(final List<T> playersList, final Comparator<T> comparator, final int limit) {
 
-		try (Reader reader = Files.newBufferedReader(Paths.get(filepath));) {
-			final ICSVBuilder<T> csvBuilder = CSVBuilderFactory.createCSVBuilder();
-			final List<T> list = csvBuilder.getLst(reader, clazz);
-			final List<T> sortedList = list.stream().sorted(comparator).distinct().collect(Collectors.toList());
+		return playersList.stream().sorted(comparator).distinct().limit(limit).collect(Collectors.toList());
 
-			return sortedList;
-
-		} catch (final IOException ioe) {
-			throw new IPLAnalyzerException(ioe.getMessage());
-		} catch (final CSVBuilderException e) {
-			throw new IPLAnalyzerException(e.getMessage());
-		}
 	}
 
-	public IPLPlayer[] getTopFiveBattingAverage() {
-		List<IPLPlayer> sortedByAverage = null;
-		try {
-			sortedByAverage = sortBy(csvPath, Comparator.comparing(IPLPlayer::getAverage).reversed(), IPLPlayer.class)
-					.stream().limit(5).collect(Collectors.toList());
+	public IPLPlayer[] getTopBattingAverage(final int limit) {
 
-		} catch (final IPLAnalyzerException e) {
-			e.getMessage();
-		}
+		final List<IPLPlayer> sortedByAverage = sortBy(batsman.getPlayerList(),
+				Comparator.comparing(IPLPlayer::getAverage).reversed(), limit);
+
 		return sortedByAverage.toArray(new IPLPlayer[0]);
 	}
 
 	public void loadBattingData(final String filepath) {
-		this.csvPath = filepath;
+		this.batsman = new PlayerRepository<>();
+		try {
+			this.batsman.build(filepath, IPLPlayer.class);
+		} catch (final IPLAnalyzerException e) {
+			e.getMessage();
+		}
 	}
 
 }
