237d19fb2845ceb0c6643904f37c416618ae9c65
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-21 12:36:21 +0530

    formatting

--- src/test/java/com/capgemini/workshop/ipl/IPLAnalyzerTest.java
+++ src/test/java/com/capgemini/workshop/ipl/IPLAnalyzerTest.java
@@ -4,12 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class IPLAnalyzerTest {
+    private static final String IPL_BATTING_DATA = "src/test/java/com/capgemini/workshop/ipl/resources/IPL2019FactsheetMostRuns.csv";
+
     @Test
     public void topBattingAverageInIPL() {
-        IPLAnalyzer iplAnalyzer=new IPLAnalyzer();
-        iplAnalyzer.loadBattingData("src/test/java/com/capgemini/workshop/ipl/resources/IPL2019FactsheetMostRuns.csv");
-        IPLPlayer[] iplPlayers= iplAnalyzer.getTopBattingAverage();
-        Assert.assertEquals(iplPlayers[0].getName(), "David Warner");
+        IPLAnalyzer iplAnalyzer = new IPLAnalyzer();
+        iplAnalyzer.loadBattingData(IPL_BATTING_DATA);
+        IPLPlayer[] iplPlayers = iplAnalyzer.getTopFiveBattingAverage();
+        Assert.assertEquals(iplPlayers[0].getName(), "MS Dhoni");
     }
-    
+
 }
